782e7d03bba00d4e154e6bba1ec1db54d09476ba
BARGAV MUNUSAMY SAMPATH <bargavsampath@tamikas-air-2.fios-router.home>
2018-06-28 18:26:45 -0400

    Arkit Cube

--- ARKit-Demo/ViewController.swift
+++ ARKit-Demo/ViewController.swift
@@ -11,7 +11,7 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -20,14 +20,31 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        //        // Create a new scene
+        //        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        //creating a 3D cube image
+        //chamferRadius: it will tell us how sharp the cube edges are
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        //creating a 3d Moon Image
+        //let moon = SCNSphere(radius: 0.2)  //radius = 20cms
+        
+        //creating an instance of SCNMaterial to give color to our cube
+        let material = SCNMaterial()
+       // material.diffuse.contents = UIImage(named: "art.scnassets/moon.jpg")
+        material.diffuse.contents = UIColor.blue
+        cube.materials = [material]
+        
+        //creating a location coordinate to place our cube: (x,y,z)
+        //z: Negative value -> towards
+        
+        let node = SCNNode()
+        node.position = SCNVector3Make(0.0, 0.0, -0.5)
+        node.geometry = cube
+        
+        //adding the cube to the rootNode of sceneView's view
+        sceneView.scene.rootNode.addChildNode(node)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,7 +52,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -50,31 +67,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
